a7e2cbac708e369d961466ef5775d93e5a4904e8
eclipse-34 <yk99902@naver.com>
2024-02-16 22:10:29 +0900

    add: Set up basic homepage structure with static files

--- web/stock_data_flow/stock_data_flow/urls.py
+++ web/stock_data_flow/stock_data_flow/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # analyticshub 앱의 모든 URL을 루트 URL에 포함
+    path('', include('analyticshub.urls')),
 ]
