3d00db76ee435ab132d47d6ffaf3df4fe3f10253
eclipse-34 <yk99902@naver.com>
2024-02-16 17:53:04 +0900

    env: Hide SECRET_KEY in environment variables

--- web/stock_data_flow/stock_data_flow/settings.py
+++ web/stock_data_flow/stock_data_flow/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
 from pathlib import Path
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # .env 파일에서 환경변수를 불러옴
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lnt&ihr6^h(s!&*z(k_rz@9t$muc!uxsxu5nlc)7b0guc&$hy1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
