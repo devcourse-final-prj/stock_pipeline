f43d9142390029505f6d3c1fc1a2d97c87698e3a
eclipse-34 <yk99902@naver.com>
2024-03-01 20:04:03 +0900

    fix: Modify css for mobile env

--- web/stock_data_flow/viz/static/viz/js/clock.js
+++ web/stock_data_flow/viz/static/viz/js/clock.js
@@ -7,7 +7,7 @@ function getClock() {
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `현재시간: ${hours}:${minutes}:${seconds}`;
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
